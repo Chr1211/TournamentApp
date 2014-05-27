7810af3b84b81bbfb76795de2af2fc952af74910
tfs <tobias.sk@gmail.com>
2014-05-27 14:00:56 +0200

    Fixed ExistingTournamentFrame GUI and fixed commit/merge error

--- TournamentAdmin/source/gui/ExistingTournamentsFrame.java
+++ TournamentAdmin/source/gui/ExistingTournamentsFrame.java
@@ -1,35 +1,29 @@
 package gui;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.DefaultListModel;
-import javax.swing.JList;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import model.Tournament;
 import service.Service;
 
 public class ExistingTournamentsFrame extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private static Service service;
 	private DefaultListModel<Tournament> model;
@@ -79,12 +73,6 @@ public class ExistingTournamentsFrame extends JFrame {
 		model = new DefaultListModel<Tournament>();
 
 		tournamentList = new JList<Tournament>(model);
-
-		
-		@SuppressWarnings("rawtypes")
-		final
-		JList tournamentList = new JList();
-
 		tournamentList.setBounds(12, 37, 206, 219);
 		contentPane.add(tournamentList);
 		JScrollPane scrollPane = new JScrollPane();
